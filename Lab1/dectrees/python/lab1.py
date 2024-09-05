3870dad5ac34f66966c38c4289e50d1c81d75700
Isac Hassle <isac@hassle.net>
2024-09-05 19:01:27 +0200

    assignment 5

--- Lab1/dectrees/python/lab1.py
+++ Lab1/dectrees/python/lab1.py
@@ -4,6 +4,7 @@ import monkdata as m
 
 
 monk_datasets = [m.monk1, m.monk2, m.monk3]
+monk_test_datasets = [m.monk1test, m.monk2test, m.monk3test]
 dataset_names = ["MONK-1", "MONK-2", "MONK-3"]
 
 
@@ -36,12 +37,33 @@ def assignment3():
     headers = ["Dataset", "a1", "a2", "a3", "a4", "a5", "a6"]
     print(tabulate(table_data, headers=headers, tablefmt="grid"))
 
+
+
+# Assignment 5
+
+def assignment5():
+
+    table_data = []
+    i = 0
+    for dataset in monk_datasets:
+        t = d.buildTree(monk_datasets[i], m.attributes)
+        table_data.append([dataset_names[i], d.check(t, monk_datasets[i]), d.check(t, monk_test_datasets[i])])
+        i += 1
+    headers = ["Dataset", "Etrain", "Etest"]
+    print(tabulate(table_data, headers=headers, tablefmt="grid"))
+
+
+# Assignment 7
+
 def main():
     # Assignment 1
     assignment1()
 
     # Assignment 3
     assignment3()
+
+    # Assignment 5
+    assignment5()
 
 if __name__ == "__main__":
     main()
