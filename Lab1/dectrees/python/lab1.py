6502d626b2df568478a2704c1b8ba6a1b2ebfeb4
Isac Hassle <isac@hassle.net>
2024-09-05 16:44:05 +0200

    assignment 3, Q4

--- Lab1/dectrees/python/lab1.py
+++ Lab1/dectrees/python/lab1.py
@@ -1,14 +1,47 @@
+from tabulate import tabulate
 import dtree as d
 import monkdata as m
 
 
-# Assignment 1
-
 monk_datasets = [m.monk1, m.monk2, m.monk3]
 dataset_names = ["MONK-1", "MONK-2", "MONK-3"]
 
-i = 0
-for dataset in monk_datasets:
-    entropy_value = d.entropy(dataset)
-    print(f"Entropy of {dataset_names[i]}: {entropy_value}")
-    i += 1
+
+# Assignment 1
+def assignment1():
+    table_data = []
+    i = 0
+    for dataset in monk_datasets:
+        table_data.append([dataset_names[i], d.entropy(dataset)])
+        i += 1
+    headers = ["Dataset", "Entropy"]
+    print(tabulate(table_data, headers=headers, tablefmt="grid"))
+        
+        
+
+
+
+# Assignment 3
+def assignment3():
+    
+    table_data = []
+    i = 0
+    for dataset in monk_datasets:
+        information_gains = [d.averageGain(dataset, attribute) for attribute in m.attributes]
+        
+        table_data.append([dataset_names[i]] + information_gains)
+        i += 1
+
+    # Print the table using tabulate
+    headers = ["Dataset", "a1", "a2", "a3", "a4", "a5", "a6"]
+    print(tabulate(table_data, headers=headers, tablefmt="grid"))
+
+def main():
+    # Assignment 1
+    assignment1()
+
+    # Assignment 3
+    assignment3()
+
+if __name__ == "__main__":
+    main()
