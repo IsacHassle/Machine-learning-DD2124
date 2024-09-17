8cde0703ccbede1cc44ebee36f57df505018e592
Isac Hassle <isac@hassle.net>
2024-09-17 14:10:29 +0200

    Added info from assignment

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -4,6 +4,25 @@ import matplotlib . pyplot as plt
 import numpy as np
 
 
+classA = np.concatenate((
+    np.random.randn(10, 2) * 0.2 + [1.5, 0.5],
+    np.random.randn(10, 2) * 0.2 + [-1.5, 0.5]
+))
+
+classB = np.random.randn(20, 2) * 0.2 + [0.0, -0.5]
+inputs = np.concatenate((classA, classB))
+targets = np.concatenate((
+    np.ones(classA.shape[0]),
+    -np.ones(classB.shape[0])
+))
+
+N = inputs.shape[0]  # Number of rows (samples)
+
+permute = list(range(N))
+random.shuffle(permute)
+inputs = inputs[permute, :]
+targets = targets[permute]
+
 def linear_kernel(x, y):
     # Linear kernel, dot product of x and y
     return np.dot(x, y)
@@ -14,18 +33,14 @@ def polynomial_kernel(x, y, p):
 
 def rbf_kernel(x, y, sigma):
     # Radial basis function (RBF) kernel
-    # K(x, y) = exp(-||x - y||^2 / (2 * sigma^2))
-    # where ||x - y|| is the Euclidean distance between x and y,
-    # and sigma controls the width of the Gaussian distribution
-    return math.exp(-linalg.norm(x-y)**2 / (2*sigma**2))
+    return np.exp(-np.linalg.norm(x-y)**2 / (2*sigma**2))
 
 
-def kernel_matrix(X, t, kernel_func):
-    n = len(X)
-    K = np.zeros((n, n))
-    for i in range(n):
-        for j in range(n):
-            K[i, j] = t[i] * t[j] * kernel_func(X[i], X[j])
+def kernel_matrix(kernel_func):
+    K = np.zeros((N, N))
+    for i in range(N):
+        for j in range(N):
+            K[i, j] = targets[i] * targets[j] * kernel_func(inputs[i], inputs[j])
     return K
 
 def objective(alpha, precomputed_K):
@@ -33,5 +48,7 @@ def objective(alpha, precomputed_K):
 
 
 def zerofun(alpha):
-    return np.sum(alpha, t)
+    return np.sum(alpha, targets)
+
+
 
